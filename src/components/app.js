4cecea66d591d8f900886354749fdaf41dd437a8
dstephenson16 <che.dami@gmail.com>
2019-12-15 20:34:35 -0700

    created footer

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Navbar from './navbar/navbar';
+import Footer from './footer/footer';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <Navbar />
+        <Footer />
       </div>
     );
   }

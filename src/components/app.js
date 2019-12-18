446be6c7e2d757a63797b13e397879b92aa55122
dstephenson16 <che.dami@gmail.com>
2019-12-18 14:29:06 -0700

    fixed loggedInStatus

--- src/components/app.js
+++ src/components/app.js
@@ -13,7 +13,7 @@ export default class App extends Component {
     super(props)
 
     this.state = {
-      loggedInStatus: true
+      loggedInStatus: false
     }
 
     this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
